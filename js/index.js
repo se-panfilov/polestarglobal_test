80324802a7536890a6945ce62ae504a6a6ae64b2
Sergey Panfilov <se-panfilov@ya.ru>
2017-02-20 03:25:56 +0800

    fixed filtering

--- js/index.js
+++ js/index.js
@@ -102,14 +102,87 @@ const table = (function (config, dom, elements) {
   }
 }(config, dom, elements))
 
+const filter = (function (elements) {
+
+  const _p = {
+    filterStrict (val, field, value) {
+      console.info(val[field])
+      return val[field] === value
+    },
+    filterNotStrict (val, field, value) {
+      return val[field].toLowerCase().includes(value.toLowerCase())
+    }
+  }
+
+  return {
+    fields: {
+      name: {
+        name: 'name',
+        defaultVal: '',
+      },
+      severity: {
+        name: 'country_check_severity',
+        defaultVal: 'all',
+        values: {
+          all: {
+            name: 'all'
+          },
+          critical: {
+            name: 'critical',
+            val: '90-CRITICAL'
+          },
+          unknown: {
+            name: 'unknown',
+            val: '30-UNKNOWN'
+          },
+          warning: {
+            name: 'warning',
+            val: '70-WARNING'
+          },
+          ok: {
+            name: 'ok',
+            val: '60-OK'
+          }
+        }
+      }
+    },
+    filterBy (data, field, value, isStrict) {
+      if (!data) throw new Error('filterBy: data shall be passed')
+      if (!field) throw new Error('filterBy: field shall be passed')
+
+      return data.filter(v => ((isStrict ? _p.filterStrict : _p.filterNotStrict))(v, field, value))
+    },
+    filterBySeverity(data, value, isStrict) {
+      if (!data) throw new Error('filterBySeverity: data shall be passed')
+
+      const isSeverityFilter = !!(value && value !== this.fields.severity.defaultVal)
+      const severityVal = this.fields.severity.values[value].val
+      if (isSeverityFilter) {
+        data = filter.filterBy(data, filter.fields.severity.name, severityVal, isStrict)
+      }
+      return data
+    },
+    getFiltersValues () {
+      return {
+        [this.fields.name.name]: elements.getNameInput().value,
+        [this.fields.severity.name]: elements.getCheckSeveritySelect().value
+      }
+    }
+  }
+}(elements))
+
 //This is a "pretend" for server work
-const fetch = (function (elements, data) {
+const fetch = (function (elements, data, filter) {
   'use strict'
 
   const _p = {
     filterData (data, filters) {
       // TODO (S.Panfilov) implement filtering
-      console.info(filters)
+      const isNameFilter = (filters.name && filters.name.length > 0)
+      if (isNameFilter) data = filter.filterBy(data, filter.fields.name.name, filters.name, false)
+
+      data = filter.filterBySeverity(data, filters[filter.fields.severity.name], true)
+
       return data
     },
     fetchData (cb) {
@@ -141,14 +214,14 @@ const fetch = (function (elements, data) {
     },
     _p // dirty hack for tests
   }
-}(elements, data))
+}(elements, data, filter))
 
 // eslint-disable-next-line no-unused-vars
-const main = (function (elements, dom, fetch, table) {
+const main = (function (elements, dom, fetch, table, filter) {
   'use strict'
 
   function onSubmit (event) {
-    const filters = getFiltersValues()
+    const filters = filter.getFiltersValues()
     fetch.onSubmit(event, filters, onGetData)
   }
 
@@ -157,24 +230,17 @@ const main = (function (elements, dom, fetch, table) {
     table.displayData(data)
   }
 
-  function getFiltersValues () {
-    return {
-      nameInput: elements.getNameInput().value,
-      severitySelect: elements.getCheckSeveritySelect().value
-    }
-  }
-
   return {
     init () {
       const formElem = elements.getFiltersForm()
 
       // TODO (S.Panfilov) replace with on change
       dom.addEventListener(formElem, 'submit', event => onSubmit(event))
-      fetch.getScreening(onGetData, getFiltersValues())
+      fetch.getScreening(onGetData, filter.getFiltersValues())
     }
   }
 
-}(elements, dom, fetch, table))
+}(elements, dom, fetch, table, filter))
 
 // for testing purpose
 if (typeof module === 'object' && module.exports) {
